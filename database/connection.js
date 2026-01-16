a1f7815affe1febfa786a9e446913a2ab92b2625
Guilherme Rodrigues <guilhermerd780@gmail.com>
2026-01-16 13:13:34 -0300

    Update database connection configuration variables

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,12 @@
 import { Sequelize } from "sequelize";
 
 const connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
     logging: false,
     timezone: "-03:00",
